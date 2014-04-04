4cd515c30cbea83c1fd0294618d6e2cc99880fa3
DeLynn Berry <delynn@gmail.com>
2014-04-04 14:08:43 -0600

    Allow file to be run with embedded sample data or with an input file.

--- ruby/001-fizz-buzz.rb
+++ ruby/001-fizz-buzz.rb
@@ -43,5 +43,29 @@ class FizzBuzz
 end
 
 if __FILE__ == $0
-  puts FizzBuzz.new(*ARGV)
+  (ARGV.first ? File.open(ARGV.first) : DATA).each_line do |line|
+    puts FizzBuzz.new(*line.split(' '))
+  end
 end
+
+__END__
+10 1 21
+20 11 100
+12 13 26
+17 9 90
+12 11 41
+15 5 29
+5 4 41
+14 5 33
+6 14 64
+5 3 99
+16 5 99
+17 4 99
+2 11 33
+10 6 75
+8 3 70
+15 12 97
+6 1 97
+20 8 99
+9 6 57
+8 6 94
