7bc109c03d9583ad42bf00a07e3c4211f5e50ea6
DeLynn Berry <delynn@gmail.com>
2014-04-04 15:56:41 -0600

    Add solution for reverse words.

---
+++ ruby/004-reverse-words.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+
+if __FILE__ == $0
+  (ARGV.first ? File.open(ARGV.first) : DATA).each_line do |line|
+    next if line.to_s.empty?
+    puts line.split(/\W/).reverse.join(' ')
+  end
+end
+
+__END__
+accusantium doloremque laudantium totam
+iste natus error sit voluptatem
+M5 b heINJVoSH1MuMkhUPr tFawbCYz hM k9R
+inventore veritatis et quasi architecto
+GE4M6Rxq 4R M 7jOLXmxDwzXnKwHVUN
+vhCplQqi o wzP Jl MTqUSgLMTjk8UTwL
+sed ut perspiciatis unde omnis
+bm6qLU Vu hFz NNX O4zzwSt pd6AgdWK U TJ ksN3PyEHz w qrf R dQftM5 F
+LRf M4tIy53l2e7puuT6gBv 5xbIDNGHrYsoBNuPj Jgfp4s W7obgb MR7o3f
+5Hb 0g8 tp f3 gHX20Dg Q2o aDfi WHTjRu8jhahlNCCo4GzC
+u 5s mHKNr3 B0b 9au1X4BLLGs 2fI9YT8J LRSlq YrnwM5 YLHl P
+tyblRwmmkINHLUCYaGq6AKuVCtWFNpyvUDp dhy
+9DuqgAM3mbcH U0Xxj G WU jvWXjRsqX GQ 7 lJ q91
+riKSomHuv IdQuu g cDOet0co FcKMs7KIp lhkWxYl7Vcyn C
+umTq2x5Bq7KHXxo7WsvBbFTV1CJhbcTkwg93nmbQ 46kd7pAYXCBDipdxlQwoDZ w gpnt
+GI4uUL iWUMPXYHj uJN HM Rwu MHvAc gh W u9N9ylLkDhW BSz1 7P563G 2qdv
+rem aperiam eaque ipsa quae ab illo
+sunt explicabo
+HkLIODAv3U2 0nKMsQ CnsR k9OgQ IgG YHzhDYWno luWX399 bnf
+V9aw yG mcOerHpOdD1DKqjEc td lQOdy7wG c6FSv0 RD HB bGFO1Mg73Py
